45aad5a4c75fbbadb61039a4b1ac904d394fa65c
TM64715 <tage.mehta@gmail.com>
2021-04-22 22:27:24 -0400

    remove icon

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,7 +1,7 @@
 const sveltePreprocess = require("svelte-preprocess");
 // eslint-disable-next-line no-unused-vars
 const pkg = require('./package.json');
-const stat = require('@sveltejs/adapter-static');
+const vercel = require('@sveltejs/adapter-vercel');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
@@ -14,7 +14,7 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: {adapt: stat},
+		adapter: vercel(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
